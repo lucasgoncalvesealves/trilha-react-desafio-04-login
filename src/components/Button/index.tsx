05c813f0a009675f6bbdc53aba14168720f20c08
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-07-10 01:01:41 -0300

    Update index.tsx
    Implementação da função de botão desabilitado.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title }: IButtonProps) => {
+  return <ButtonContainer>{title}</ButtonContainer>;
 };
 
 export default Button;
