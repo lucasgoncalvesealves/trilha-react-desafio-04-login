5d70f70a348f65b3b7f2dc86e73ff67ece620da9
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-07-10 00:59:54 -0300

    Update index.tsx
    Implementação da função de botão desabilitado.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,7 +9,10 @@ import { defaultValues, IFormLogin } from "./types";
 
 const schema = yup
   .object({
-    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    email: yup
+      .string()
+      .email("E-mail inválido")
+      .required("Campo obrigatório"),
     password: yup
       .string()
       .min(6, "No minimo 6 caracteres")
@@ -49,7 +52,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar" disabled={isValid} />
         </Column>
       </LoginContainer>
     </Container>
